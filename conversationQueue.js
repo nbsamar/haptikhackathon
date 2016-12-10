64962dbd3be0e6deb24f041f81d9bafc0248b089
nbsamar <nbsamar@gmail.com>
2016-12-11 03:53:54 +0530

    Adding last two

--- conversationQueue.js
+++ conversationQueue.js
@@ -37,7 +37,7 @@ module.exports = function(apiParams){
      end_point_callback:function(text,bot,message) {
         apiParams.BloodPressure = parseFloat(text)
         console.log(text)
-        bot.reply(message,'Thanks for your response')
+        
      }},
      {
      message:'What is your Skin Thickness?',
@@ -45,7 +45,7 @@ module.exports = function(apiParams){
      end_point_callback:function(text,bot,message) {
         apiParams.SkinThickness = parseFloat(text)
         console.log(text)
-        bot.reply(message,'Thanks for your response')
+        
      }},
       {
      message:'What is your Age?',
